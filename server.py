a03da25dc741d5113bd31bd46c7562c3fce5f624
ArtemBartender <Bilenckotema10@gmail.com>
2025-11-06 11:32:25 +0100

    Update server.py

--- server.py
+++ server.py
@@ -799,7 +799,7 @@ def me_settings_set():
 #------------------------Zmiana Hasła-----------------------------------
 # ========== Смена пароля до входа (на странице логина) ==========
 
-from app import app, db
+from server import app, db
 from models import User
 from flask_bcrypt import Bcrypt
 
@@ -3156,36 +3156,5 @@ if __name__ == '__main__':
         ensure_coord_lounge_column()
         ensure_lounge_column()   # ← ВАЖНО
     app.run(host='0.0.0.0', port=port, debug=True, use_reloader=False)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
